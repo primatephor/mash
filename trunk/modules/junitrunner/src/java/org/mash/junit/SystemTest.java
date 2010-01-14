b2ba18e44c23cb07a87b5f3eaa71380b715eb296
teastlack <teastlack@eb3ecde4-7618-11de-b058-93adefbabda0>
2010-01-14 01:12:16 +0000

    add tag based sensitivity

--- trunk/modules/junitrunner/src/java/org/mash/junit/SystemTest.java
+++ trunk/modules/junitrunner/src/java/org/mash/junit/SystemTest.java
@@ -7,11 +7,14 @@ import org.mash.config.Suite;
 import org.mash.loader.SuiteLoader;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Use Ant to invoke the system test.
  * <p/>
  * To specify the test suite file, set the system property "system.test.file".
+ * To specify the list of tags, supply the system proptery "test.tags" as a comma separated list of tags.
  * <p/>
  * To change the marshaller (you can build your own, no need for xml) set the system property "suite.marshaller"
  * <p/>
@@ -19,8 +22,9 @@ import java.util.List;
  */
 public class SystemTest
 {
-    private static final Logger logger = Logger.getLogger(SystemTest.class);
+    private static final Logger log = Logger.getLogger(SystemTest.class);
     private static String SYSTEM_TEST = System.getProperty("system.test.file", "SystemTest.xml");
+    private static String TAGS = System.getProperty("test.tags", "");
 
     /**
      * Create the test suite by loading the system test file defined by "system.test.file"
@@ -30,12 +34,12 @@ public class SystemTest
      */
     public static Test suite() throws Exception
     {
-        logger.debug("Preparing to load system test(s) from context " + SYSTEM_TEST);
+        log.debug("Preparing to load system test(s) from context " + SYSTEM_TEST);
 
         Suite suite = new SuiteLoader().loadSuite(SYSTEM_TEST);
         TestSuite testSuite = new TestSuite();
         JunitTestBuilder builder = new JunitTestBuilder();
-        List<Test> tests = builder.buildTests(suite);
+        List<Test> tests = builder.buildTests(suite, getTags());
         for (Test test : tests)
         {
             testSuite.addTest(test);
@@ -44,5 +48,18 @@ public class SystemTest
         return testSuite;
     }
 
-
+    public static List<String> getTags()
+    {
+        List<String> tags = new ArrayList<String>();
+        if(TAGS != null && TAGS.length() > 0)
+        {
+            log.info("Running tests with tags:"+TAGS);
+            String[] tagArray = TAGS.split(",");
+            if(tagArray != null && tagArray.length > 0)
+            {
+                tags.addAll(Arrays.asList(tagArray));
+            }
+        }
+        return tags;
+    }
 }
