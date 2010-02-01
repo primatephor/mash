bfdca4b1f24884cc4c01bc34749911b0bc1e56bc
teastlack <teastlack@eb3ecde4-7618-11de-b058-93adefbabda0>
2010-02-01 18:42:31 +0000

    tests for object messages

---
+++ trunk/modules/messageharness/test/java/org/mash/harness/jms/MyObjectMessage.java
@@ -0,0 +1,41 @@
+package org.mash.harness.jms;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author teastlack
+ * @since Feb 1, 2010 10:34:19 AM
+ *
+ */
+public class MyObjectMessage implements Serializable
+{
+    private String name;
+    private String value;
+
+    public MyObjectMessage(String name, String value)
+    {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+
+    public void setValue(String value)
+    {
+        this.value = value;
+    }
+}
