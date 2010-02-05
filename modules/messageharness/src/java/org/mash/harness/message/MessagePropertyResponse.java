45170d1f1e819e9fbfeef0fd3dfdfdb0efbcf6ac
teastlack <teastlack@eb3ecde4-7618-11de-b058-93adefbabda0>
2010-02-05 19:10:40 +0000

    clean up harnesses to behave in a more 'general' way

---
+++ modules/messageharness/src/java/org/mash/harness/message/MessagePropertyResponse.java
@@ -0,0 +1,71 @@
+package org.mash.harness.message;
+
+import org.apache.log4j.Logger;
+import org.mash.harness.RunResponse;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * provides access to message properties for verification
+ * 
+ * @author teastlack
+ * @since Jan 29, 2010 3:19:20 PM
+ *
+ */
+public class MessagePropertyResponse implements RunResponse
+{
+    private static final Logger log = Logger.getLogger(MessagePropertyResponse.class.getName());
+
+    private RunResponse target;
+    private Map<String, String> properties;
+
+    public MessagePropertyResponse(RunResponse target,
+                                   Map<String, String> properties)
+    {
+        this.target = target;
+        this.properties = properties;
+    }
+
+    public Map<String, String> getProperties()
+    {
+        if (properties == null)
+        {
+            properties = new HashMap<String, String>();
+        }
+        return properties;
+    }
+
+    public String getValue(String name)
+    {
+        String result = getProperties().get(name);
+        if (result == null)
+        {
+            try
+            {
+                result = target.getValue(name);
+            }
+            catch (Exception e)
+            {
+                log.error("Unexpected error calling target getValue", e);
+            }
+        }
+        return result;
+    }
+
+    public Collection<String> getValues(String name)
+    {
+        return target.getValues(name);
+    }
+
+    public Collection<String> getValues()
+    {
+        return target.getValues();
+    }
+
+    public String getString()
+    {
+        return target.getString();
+    }
+}
