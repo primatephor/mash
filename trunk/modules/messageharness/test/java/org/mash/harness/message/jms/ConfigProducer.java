376f9b956c41862c2b0af10a1bbd93a7e1fb89e5
teastlack <teastlack@eb3ecde4-7618-11de-b058-93adefbabda0>
2010-02-05 19:09:16 +0000

    clean up harnesses to behave in a more 'general' way

---
+++ trunk/modules/messageharness/test/java/org/mash/harness/message/jms/ConfigProducer.java
@@ -0,0 +1,102 @@
+package org.mash.harness.message.jms;
+
+import javax.jms.MessageProducer;
+import javax.jms.JMSException;
+import javax.jms.Destination;
+import javax.jms.Message;
+
+/**
+ *
+ * @author teastlack
+ * @since Feb 1, 2010 9:37:45 AM
+ *
+ */
+public class ConfigProducer implements MessageProducer
+{
+    Message message;
+
+    public Message getMessage()
+    {
+        return message;
+    }
+
+    public void setDisableMessageID(boolean b) throws JMSException
+    {
+        throw new UnsupportedOperationException("Method setDisableMessageID not yet implemented");
+    }
+
+    public boolean getDisableMessageID() throws JMSException
+    {
+        throw new UnsupportedOperationException("Method getDisableMessageID not yet implemented");
+    }
+
+    public void setDisableMessageTimestamp(boolean b) throws JMSException
+    {
+        throw new UnsupportedOperationException("Method setDisableMessageTimestamp not yet implemented");
+    }
+
+    public boolean getDisableMessageTimestamp() throws JMSException
+    {
+        throw new UnsupportedOperationException("Method getDisableMessageTimestamp not yet implemented");
+    }
+
+    public void setDeliveryMode(int i) throws JMSException
+    {
+        throw new UnsupportedOperationException("Method setDeliveryMode not yet implemented");
+    }
+
+    public int getDeliveryMode() throws JMSException
+    {
+        throw new UnsupportedOperationException("Method getDeliveryMode not yet implemented");
+    }
+
+    public void setPriority(int i) throws JMSException
+    {
+        throw new UnsupportedOperationException("Method setPriority not yet implemented");
+    }
+
+    public int getPriority() throws JMSException
+    {
+        throw new UnsupportedOperationException("Method getPriority not yet implemented");
+    }
+
+    public void setTimeToLive(long l) throws JMSException
+    {
+        throw new UnsupportedOperationException("Method setTimeToLive not yet implemented");
+    }
+
+    public long getTimeToLive() throws JMSException
+    {
+        throw new UnsupportedOperationException("Method getTimeToLive not yet implemented");
+    }
+
+    public Destination getDestination() throws JMSException
+    {
+        throw new UnsupportedOperationException("Method getDestination not yet implemented");
+    }
+
+    public void close() throws JMSException
+    {
+        throw new UnsupportedOperationException("Method close not yet implemented");
+    }
+
+    public void send(Message message) throws JMSException
+    {
+        this.message = message;
+    }
+
+    public void send(Message message, int i, int i1, long l) throws JMSException
+    {
+        this.message = message;
+    }
+
+    public void send(Destination destination, Message message) throws JMSException
+    {
+        this.message = message;
+    }
+
+    public void send(Destination destination, Message message, int i, int i1, long l) throws JMSException
+    {
+        this.message = message;
+    }
+}
