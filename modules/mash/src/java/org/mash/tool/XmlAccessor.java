fc9da6cc68801a7be742ef8cfd63fee2e1a721c7
now2dmax <now2dmax@eb3ecde4-7618-11de-b058-93adefbabda0>
2010-10-27 21:21:39 +0000

    fixing stack overflow

--- modules/mash/src/java/org/mash/tool/XmlAccessor.java
+++ modules/mash/src/java/org/mash/tool/XmlAccessor.java
@@ -5,20 +5,18 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXParseException;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 
 /**
- *
  * @author teastlack
  * @since Sep 17, 2009 6:07:28 PM
- *
  */
 public class XmlAccessor
 {
@@ -179,56 +177,6 @@ public class XmlAccessor
 
     private String removeNamespaces(String xml)
     {
-        StringBuffer response = new StringBuffer();
-        int startElement = xml.indexOf("<");
-        int endElement = xml.indexOf(">");
-        int nextName = xml.indexOf(":");
-        int nextSpace = xml.indexOf(" ");
-
-        if (startElement < 0)
-        {
-            response.append(xml);
-        }
-        else
-        {
-            //we're within an element
-            if (startElement < nextName && nextName < endElement)
-            {
-                //is there a space before the ':', indicating
-                //possible namespace definition, if so we ignore
-                if (nextSpace < 0 || nextSpace > nextName)
-                {
-                    int startoffset = 1;
-                    if (xml.charAt(startElement + 1) == '/')
-                    {
-                        startoffset = 2;
-                    }
-                    response.append(xml.substring(0, startElement + startoffset));
-                    response.append(xml.substring(nextName + 1, endElement + 1));
-                }
-                else
-                {
-                    response.append(xml.substring(0, endElement + 1));
-                }
-            }
-            else
-            {
-                response.append(xml.substring(0, endElement + 1));
-            }
-
-            //move to next element
-            xml = xml.substring(endElement + 1);
-            int nextStartElement = xml.indexOf("<");
-            if (nextStartElement >= 0)
-            {
-                response.append(xml.substring(0, nextStartElement));
-                response.append(removeNamespaces(xml.substring(nextStartElement)));
-            }
-            else
-            {
-                response.append(xml);
-            }
-        }
-        return response.toString();
+        return xml.replaceAll("(</?)[^:> ]+:", "$1");
     }
 }
