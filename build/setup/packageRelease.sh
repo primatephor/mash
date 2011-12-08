08e3742c2ab2b492a20b1b0ca85b3245ba51a496
teastlack <teastlack@eb3ecde4-7618-11de-b058-93adefbabda0>
2011-12-08 01:02:39 +0000

    attempting to get a maven assembly

--- build/setup/packageRelease.sh
+++ build/setup/packageRelease.sh
@@ -6,42 +6,17 @@ then
   exit
 fi
 
-RELEASE_NAME=mash-"$1"
+RELEASE_NAME=$1
 
 cd ..
 BUILD_DIR=`pwd`
 rm -rf $RELEASE_NAME
+rm -rf target
 
-mkdir $BUILD_DIR/$RELEASE_NAME
+#mkdir $BUILD_DIR/$RELEASE_NAME
 
 ## BUILD IT ALL
-cd ../modules
-ant clean
-ant build
-ant docs
 
-cd $BUILD_DIR
-cp -R target/* $RELEASE_NAME
-
-## Move the readme
-cp setup/readme.txt $RELEASE_NAME/readme.txt
-
-## Copy source
-mkdir $RELEASE_NAME/src
-mkdir $RELEASE_NAME/src/mash
-mkdir $RELEASE_NAME/src/junitrunner
-mkdir $RELEASE_NAME/src/dbharness
-mkdir $RELEASE_NAME/src/httpharness
-cp -R ../modules/mash/src/* $RELEASE_NAME/src/mash
-cp -R ../modules/junitrunner/src/* $RELEASE_NAME/src/junitrunner
-cp -R ../modules/dbharness/src/* $RELEASE_NAME/src/dbharness
-cp -R ../modules/httpharness/src/* $RELEASE_NAME/src/httpharness
-
-## Clean it up
-cd $RELEASE_NAME
-find . -name ".svn" -exec rm -rf {} \;
-
-## Zip it all up
-cd $BUILD_DIR
-zip -r $RELEASE_NAME.zip $RELEASE_NAME
+mvn -am -pl org.mash:Mash clean install -Dmaven.test.skip=true
+mvn -pl org.mash:Mash -Dversion=$RELEASE_NAME assembly:assembly
 
