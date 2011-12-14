34fa00de3790ab0e38b8405bc6bd8c067f8ae1ac
teastlack <teastlack@eb3ecde4-7618-11de-b058-93adefbabda0>
2011-12-14 01:12:27 +0000

    start selenium server integration

---
+++ trunk/modules/seleniumharness/test/java/org/mash/harness/selenium/TestServerSample.java
@@ -0,0 +1,15 @@
+package org.mash.harness.selenium;
+
+import junit.framework.TestCase;
+
+/**
+ * @author teastlack
+ * @since 12/13/11 4:56 PM
+ */
+public class TestServerSample extends TestCase
+{
+    public void testServerSetup()
+    {
+
+    }
+}
