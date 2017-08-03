ae304e41846ee467a3a2251d877f4ece59d41c25
Primatephor <primatephor@gmail.com>
2017-08-03 14:38:13 -0700

    fix NPE

--- modules/mash/src/java/org/mash/harness/StandardVerifyHarness.java
+++ modules/mash/src/java/org/mash/harness/StandardVerifyHarness.java
@@ -39,7 +39,7 @@ public class StandardVerifyHarness extends BaseHarness implements VerifyHarness
         RunResponse response = run.getResponse();
         if (log.isDebugEnabled())
         {
-            log.debug("Verifying response :" + response.toString());
+            log.debug("Verifying response :" + (response != null? response.toString():null));
         }
 
         if (response != null)
