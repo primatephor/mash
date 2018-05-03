ce50d27e95a38ddf59cc75efe6c37de72d13a746
primatephor <primatephor@gmail.com>
2018-05-02 22:45:54 -0700

    add port parsing to ftp url

--- modules/ftpharness/src/java/org/mash/harness/ftp/FTPRunHarness.java
+++ modules/ftpharness/src/java/org/mash/harness/ftp/FTPRunHarness.java
@@ -16,7 +16,7 @@ import org.mash.loader.HarnessParameter;
 
 /**
  * Perform generic FTP operations on a server, based on whatever parameters are passed via the ftp_params param.
- *
+ * <p>
  * Configurations:
  * <ul>
  * <li> 'url' is the url to submit to </li>
@@ -31,11 +31,9 @@ import org.mash.loader.HarnessParameter;
  * </ul>
  *
  * @since Sep 17, 2009 10:20:40 AM
- *
  */
 @HarnessName(name = "ftp")
-public class FTPRunHarness extends BaseHarness implements RunHarness
-{
+public class FTPRunHarness extends BaseHarness implements RunHarness {
     private static final Logger log = LogManager.getLogger(FTPRunHarness.class.getName());
     //configs
     private String url;
@@ -47,121 +45,99 @@ public class FTPRunHarness extends BaseHarness implements RunHarness
 
     private RunResponse response;
 
-    public void run(HarnessContext context)
-    {
+    public void run(HarnessContext context) {
         FTPClient client = buildClient();
-        try
-        {
+        try {
             //5 minutes
-            client.setConnectTimeout(5*60*1000);
+            client.setConnectTimeout(5 * 60 * 1000);
             log.info("Attempting connect to " + url);
-            client.connect(url);
+            int port = 22;
+            if (url.contains(":")) {
+                int portIdx = url.indexOf(":");
+                port = Integer.valueOf(url.substring(portIdx + 1));
+                url = url.substring(0, portIdx);
+            }
+            client.connect(url, port);
             int reply = client.getReplyCode();
             log.info("Connection status: " + reply);
 
             // After connection attempt, you should check the reply code to verify success.
-            if (!FTPReply.isPositiveCompletion(reply))
-            {
+            if (!FTPReply.isPositiveCompletion(reply)) {
                 client.disconnect();
                 log.error("FTP connection refused");
                 this.getErrors().add(new HarnessError(this, "Connection", "FTP connection refused"));
             }
 
-            if (!hasErrors())
-            {
-                if (!client.login(user, password))
-                {
+            if (!hasErrors()) {
+                if (!client.login(user, password)) {
                     this.getErrors().add(new HarnessError(this, "Authentication", "Unable to login with user " + user));
-                }
-                else
-                {
+                } else {
                     response = runOperation(client);
                 }
-                if (response != null && log.isTraceEnabled())
-                {
+                if (response != null && log.isTraceEnabled()) {
                     log.trace("Response:" + response.getString());
                 }
                 client.logout();
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             log.error("Unexpected error ftping server", e);
             this.getErrors().add(new HarnessError(this.getClass().getName(),
-                                                  "Unexpected error ftping server", e.getMessage()));
-        }
-        finally
-        {
-            if (client.isConnected())
-            {
-                try
-                {
+                    "Unexpected error ftping server", e.getMessage()));
+        } finally {
+            if (client.isConnected()) {
+                try {
                     client.disconnect();
-                }
-                catch (Exception ioe)
-                {
+                } catch (Exception ioe) {
                     log.error("Unexpected error closing connection to ftp server", ioe);
                 }
             }
         }
     }
 
-    protected RunResponse runOperation(FTPClient client) throws OperationException
-    {
+    protected RunResponse runOperation(FTPClient client) throws OperationException {
         RunResponse result = null;
-        try
-        {
-            if (ftpParams != null)
-            {
+        try {
+            if (ftpParams != null) {
                 String command = ftpParams;
                 String args = null;
-                if (command.indexOf(" ") > 0)
-                {
+                if (command.indexOf(" ") > 0) {
                     command = command.substring(0, command.indexOf(" "));
                     args = ftpParams.substring(ftpParams.indexOf(" ") + 1);
                 }
                 client.sendCommand(command, args);
                 result = new RawResponse(client.getReplyStrings());
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new OperationException("Problem running command remotely", e);
         }
         return result;
     }
 
-    protected FTPClient buildClient()
-    {
+    protected FTPClient buildClient() {
         return new FTPClient();
     }
 
-    public RunResponse getResponse()
-    {
+    public RunResponse getResponse() {
         return response;
     }
 
     @HarnessConfiguration(name = "url")
-    public void setUrl(String url)
-    {
+    public void setUrl(String url) {
         this.url = url;
     }
 
     @HarnessConfiguration(name = "user")
-    public void setUser(String user)
-    {
+    public void setUser(String user) {
         this.user = user;
     }
 
     @HarnessConfiguration(name = "password")
-    public void setPassword(String password)
-    {
+    public void setPassword(String password) {
         this.password = password;
     }
 
     @HarnessParameter(name = "ftp_params")
-    public void setFtpParams(String ftpParams)
-    {
+    public void setFtpParams(String ftpParams) {
         this.ftpParams = ftpParams;
     }
 
